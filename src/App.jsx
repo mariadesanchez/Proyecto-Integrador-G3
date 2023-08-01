5fd919fae84c30f987a159111958fb8176589c27
mariade_snchz <calder07@gmail.com>
2023-08-01 03:09:26 -0300

    Header, Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Route, Routes } from 'react-router-dom'
+import Home from './Routes/Home'
+import PageNotFound from './Routes/PageNotFound'
+import { routes } from './utils/routes'
+import Detail from './Routes/Detail'
+import Layout from './Layout/Layout'
+import { usecontextGlobal } from './Components/utils/GlobalContext'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const dentistState = {dentistTheme:true}
+  // eslint-disable-next-line no-unused-vars
+  const {dentistState} = usecontextGlobal()
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+
+         
+           <Routes>
+      
+      
+           <Route path='/' element={<Layout/>}>
+           <Route path={routes.home} element={<Home/>}/>
+           <Route path={routes.detail} element={<Detail/>}/>
+           <Route path={routes.pageNotFound} element={<PageNotFound/>}/>
+         </Route>
+       
+          </Routes>
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Routes/Home.jsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Card from '../Components/Card'
+import { usecontextGlobal } from '../Components/utils/GlobalContext.jsx'
+
+
+
+
+const Home = () => {
+  // eslint-disable-next-line no-unused-vars
+  const {dentistState} = usecontextGlobal()
+  return (
+    <main className="" >
+      <h1>Home</h1>
+      <div className='card-grid'>
+        {/* Aqui deberias renderizar las cards */}
+      {/* <div className='item'> */}
+        {dentistState.dentistsList.map(dentist => (<Card dentist={dentist} key ={dentist.id}/>))}
+     {/* </div> */}
+      </div>
+    </main>
+  )
+}
+
+export default Home
