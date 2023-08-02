7baba3d63f2364a46a8315f0370e2ea42fadab4d
LeandroF2022 <farias371@gmail.com>
2023-08-02 17:04:53 -0300

    Actualizacion del Header y Card

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,17 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from '../Components/Header'
+import Navbar from '../Components/Header/Header'
 import Footer from '../Components/Footer'
 
 const Layout = () => {
- 
-
-
-  return (
+return (
     
     <div >
-     
         <Navbar/>
         <Outlet/>
         <Footer/>
